6ee3911b507e63423df7d15e3c1d8144351bb2b8
yusupovna1403 <yusupovna2003girl@gmail.com>
2022-12-20 22:27:51 +0500

    build func07

--- build_func07.py
+++ build_func07.py
@@ -9,4 +9,6 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    answer = pow(x , 2) + 6 * pow(x , 3) + 3 * x * y
+    return answer
+print(main(5 , 2))
